d94c23a22f25960679edadfefb41c8d9d2ab48c6
cauprkprj <comnamu18@gmail.com>
2018-02-07 15:09:51 +0900

    not

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,3 +5,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
 ]
+
+if settings.DEBUG:
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
