6d75867ea966db9760c978a064b664b7b6321585
cauprkprj <comnamu18@gmail.com>
2018-02-11 17:36:50 +0900

    fix indentation error

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
