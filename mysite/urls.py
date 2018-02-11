3ac63f116c5900b24120f83ba10e6887ac152aeb
cauprkprj <comnamu18@gmail.com>
2018-02-11 17:41:14 +0900

    weweel

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -7,4 +8,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
