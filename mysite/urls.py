0e4d1220b51921ceaf37d10dfded2bb6fcf1756c
cauprkprj <comnamu18@gmail.com>
2018-02-11 18:21:51 +0900

    wjlwerjl

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url, static
 from django.contrib import admin
 from django.conf import settings
 
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
