1434d394417e32e41057520f6a277693bedb71ca
cauprkprj <comnamu18@gmail.com>
2018-02-20 15:04:05 +0900

    wow

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,6 +7,3 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
